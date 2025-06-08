6c2ea5d757723015404aa680eeae5e2f8a656a10
Wocked61 <DylanP@csu.fullerton.edu>
2025-06-07 23:17:22 -0700

    added additonal files for header and footer

---
+++ portfolio_React/src/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+function Footer() {
+  return (
+    <footer className="App-footer">
+      <p>&copy; 2025 Your Name</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ portfolio_React/src/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css'
+
+function Header() {
+  return (
+    <header className="App-header">
+      <h1>Welcome to My Portfolio</h1>
+      <p>This is a simple React application.</p>
+    </header>
+  )
+}
+
+export default Header
