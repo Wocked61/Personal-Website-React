64ec0596d7398f1b4f74af9403bd17ed05d48fb0
Wocked61 <DylanP@csu.fullerton.edu>
2025-06-26 02:46:41 -0700

    fixed errors

--- portfolio_React/vite.config.js
+++ portfolio_React/vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Personal-Website-React-/',
-  build: {
-    outDir: 'dist'
-  }
+  base: '/Personal-Website-React/'
 })
