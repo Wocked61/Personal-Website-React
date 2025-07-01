62904431040113f71cc2ce10a3c1739066fa66da
Wocked61 <DylanP@csu.fullerton.edu>
2025-07-01 01:50:35 -0700

    fixed small typo

--- portfolio_React/vite.config.js
+++ portfolio_React/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Personal-Website-React-/',
+  base: '/Personal-Website-React/',
   build: {
     outDir: 'dist'
   }
